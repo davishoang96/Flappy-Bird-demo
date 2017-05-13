ecc130b7fd2aaf878af81e3d9861d4e802b2805a
Hoang Viet <davishoang96@gmail.com>
2017-05-13 19:20:35 +0700

    Pipe spawner

---
+++ Assets/scripts/pipeSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject pipeScoreHolder;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Spawner());
+	}
+	
+
+    IEnumerator Spawner()
+    {
+        yield return new WaitForSeconds(1);
+        Instantiate(pipeScoreHolder, transform.position, Quaternion.identity);
+        StartCoroutine(Spawner());
+    }
+}
