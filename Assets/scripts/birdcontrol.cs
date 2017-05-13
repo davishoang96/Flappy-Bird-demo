b05164cc6215312a9b5e75899f864ebbb0f5a6b1
Hoang Viet <davishoang96@gmail.com>
2017-05-13 18:39:55 +0700

    Initiation

---
+++ Assets/scripts/birdcontrol.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class birdcontrol : MonoBehaviour {
+
+    public float bounceForce;
+    private Rigidbody2D myBody;
+    private Animator anim;
+
+    private bool isAlive;
+    private bool didFlap;
+
+	// Use this for initialization
+	void Awake () {
+        isAlive = true;
+
+		myBody = GetComponent<Rigidbody2D> ();
+
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        BirdMovement();
+	}
+
+    void BirdMovement()
+    {
+        if(isAlive == true)
+        {
+            if(didFlap)
+            {
+                didFlap = false;
+                myBody.velocity = new Vector2(myBody.velocity.x, bounceForce);
+            }
+        }
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    myBody.velocity = new Vector2(myBody.velocity.x, bounceForce);
+        //}
+
+        if (myBody.velocity.y > 0)
+        {
+            float angel = Mathf.Lerp(0, 45, myBody.velocity.y / 7);
+            transform.rotation = Quaternion.Euler(0, 0, angel);
+        } 
+        else if (myBody.velocity.y == 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (myBody.velocity.y < 0)
+        {
+            float angel = Mathf.Lerp(0, -45, -myBody.velocity.y / 7);
+            transform.rotation = Quaternion.Euler(0, 0, angel);
+        }
+    }
+
+    public void FlapButton()
+    {
+        didFlap = true;
+    }
+}
---
+++ Assets/scripts/pipeControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeControl : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pipeMovement();
+	}
+
+    void pipeMovement()
+    {
+        Vector3 temp = transform.position;
+        temp.x = temp.x - speed * Time.deltaTime;
+        transform.position = temp;
+    }
+}
